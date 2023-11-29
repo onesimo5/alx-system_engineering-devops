5d5cb6c57f4e8d54a665edf1f961803e8cf14a0c
onesimo5 <onesimojora57@gmail.com>
2023-11-29 06:14:24 -0800

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 # using puppet to connect without password
 
 file { '/etc/ssh/ssh_config':
@@ -6,13 +6,13 @@ file { '/etc/ssh/ssh_config':
 }
 
 file_line { 'Turn off passwd auth':
- path	=> '/etc/ssh/ssh_config',
- line	=> 'PasswordAuthentication no',
- match	=> '^#PasswordAuthentication',
+ path => '/etc/ssh/ssh_config',
+ line => 'PasswordAuthentication no',
+ match => '^#PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
- path	=> '/etc/ssh/ssh_config',
- line	=> 'IdentityFile ~/.ssh/school',
- match	=> '^#IdentityFile',
+ path => '/etc/ssh/ssh_config',
+ line => 'IdentityFile ~/.ssh/school',
+ match => '^#IdentityFile',
 }
