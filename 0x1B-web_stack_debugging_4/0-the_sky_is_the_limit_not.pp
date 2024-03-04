ff3bf5a001353f9a1b2786151c14b5acfb62c95a
onesimo5 <onesimojora57@gmail.com>
2024-03-04 14:55:42 +0000

    new file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,6 @@
-# increases the amount of traffic an nginx server can handle
-
-#increase the ULIMIT of the default file
+# Fixes an nginx site that can't handle multiple concurrent requests
 exec { 'fix--for-nginx':
-   # modify the ULIMIT value 
-   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-   # specify the path for the sed command
-   path    => 'usr/local/bin/:/bin/',
-}
-
-# restart nginx
-exec { 'nginx-restart':
-  # Restart nginx service 
-  command => '/etc/init.d/nginx restart'
-  # specify the path for the the init.d script
-  path    => '/etc/init.d/',
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
